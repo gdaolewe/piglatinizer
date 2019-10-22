796688592003d2ee1be3ecdff3abbabbc2dfd759
George Daole-Wellman <gdaolewellman@gmail.com>
2019-10-22 21:16:18 +0000

    Make it work

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
-
-
-
-  
-  
-  
-  
+    $('#button').click(function() {
+        let original = $('#input').val();
+        let result = original.slice(1) + original[0] + 'ay';
+        $('.output').text(result);
+    });
 });
